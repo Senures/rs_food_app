b59a340b157670a6d815538a9de4f670f1662da2
Senures <eserlersemanur@gmail.com>
2022-10-11 21:37:47 +0300

    sayfaların iskeleti oluşturuldu.

---
+++ lib/pages/restaurant_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RestaurantScreen extends StatefulWidget {
+  RestaurantScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends State<RestaurantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
